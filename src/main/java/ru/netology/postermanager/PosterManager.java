8ad3ceef38512270339a995bea8497e359ae108a
boshih <artem.bolshih@mail.ru>
2022-06-23 19:18:13 +0300

    reworked

--- src/main/java/ru/netology/postermanager/PosterManager.java
+++ src/main/java/ru/netology/postermanager/PosterManager.java
@@ -3,7 +3,12 @@ package ru.netology.postermanager;
 public class PosterManager {
     private PostersData[] posters = new PostersData[0];
     private int lastCount = 10;
+    public PosterManager (){
 
+    }
+    public PosterManager(int lastCount){
+        this.lastCount = lastCount;
+    }
     public void addPoster(PostersData poster){
         PostersData[] tmp = new PostersData[posters.length + 1];
         for (int i = 0; i < posters.length; i++){
@@ -13,38 +18,27 @@ public class PosterManager {
         posters = tmp;
     }
 
-    public void findAll(){
-        for (PostersData poster : posters){
-            System.out.println(poster.getFilmNumber() + " " + poster.getFilmTitle() + " " + poster.getFilmTipe());
+    public PostersData[] findAll(){
+        return posters;
         }
-    }
+
 
     public PostersData[] findLast(){
-        PostersData [] all = getPosters();
-        PostersData [] lasts = new PostersData[lastCount];
-            for (int i = 0; i < lasts.length; i++){
+        PostersData [] all = findAll();
+        int lastLength = 0;
+
+        if (all.length >= lastCount){
+            lastLength = lastCount;
+        }
+        else{
+            lastLength = all.length;
+        }
+
+        PostersData [] lasts = new PostersData[lastLength];
+
+            for (int i = 0; i < lasts.length; i++) {
                 lasts[i] = all[all.length - 1 - i];
             }
-                        for (PostersData last : lasts){
-                System.out.println(last.getFilmNumber() + " " + last.getFilmTitle() + " " + last.getFilmTipe());
-            }
         return lasts;
     }
-
-    public PostersData[] findLast(int lastCount){
-        this.lastCount=lastCount;
-        PostersData [] all = getPosters();
-        PostersData [] lasts = new PostersData[lastCount];
-        for (int i = 0; i < lasts.length; i++){
-            lasts[i] = all[all.length - 1 - i];
-        }
-        for (PostersData last : lasts){
-            System.out.println(last.getFilmNumber() + " " + last.getFilmTitle() + " " + last.getFilmTipe());
-        }
-        return lasts;
-    }
-    public PostersData[] getPosters(){
-            return posters;
-    }
-
 }
--- src/test/java/ru/netology/postermanager/PosterManageTest.java
+++ src/test/java/ru/netology/postermanager/PosterManageTest.java
@@ -4,7 +4,9 @@ package ru.netology.postermanager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+
 public class PosterManageTest {
+
         PostersData poster1 = new PostersData(1, "Бладшот", "боевик");
         PostersData poster2 = new PostersData(2, "Вперёд", "мультфильм");
         PostersData poster3 = new PostersData(3, "Джентельмены", "боевик");
@@ -17,11 +19,11 @@ public class PosterManageTest {
         PostersData poster10 = new PostersData(10, "Куб", "ужасы");
         PostersData poster11 = new PostersData(11, "Смурфики", "мультфильм");
 
-
     @Test
 
     public void shoulAddPoster(){
         PosterManager poster = new PosterManager();
+
         poster.addPoster(poster1);
         poster.addPoster(poster2);
         poster.addPoster(poster3);
@@ -34,9 +36,8 @@ public class PosterManageTest {
         poster.addPoster(poster10);
         poster.addPoster(poster11);
 
-
-        PostersData[] expected = {poster1, poster2, poster3, poster4, poster5, poster6, poster7, poster8, poster9, poster10, poster11};
-        PostersData[] actual = poster.getPosters();
+       PostersData[] expected = {poster1, poster2, poster3, poster4, poster5, poster6, poster7, poster8, poster9, poster10, poster11};
+       PostersData[] actual = poster.findAll();
 
         Assertions.assertArrayEquals(expected, actual);
     }
@@ -45,6 +46,7 @@ public class PosterManageTest {
 
     public void shouldFindLast(){
         PosterManager poster = new PosterManager();
+
         poster.addPoster(poster1);
         poster.addPoster(poster2);
         poster.addPoster(poster3);
@@ -59,12 +61,15 @@ public class PosterManageTest {
 
         PostersData[] expected = {poster11, poster10, poster9, poster8, poster7, poster6, poster5, poster4, poster3, poster2};
         PostersData [] actual = poster.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
 
-    public void shouldFindLast5(){
-        PosterManager poster = new PosterManager();
+   public void shouldFindLast5(){
+        PosterManager poster = new PosterManager(5);
+
         poster.addPoster(poster1);
         poster.addPoster(poster2);
         poster.addPoster(poster3);
@@ -78,6 +83,54 @@ public class PosterManageTest {
         poster.addPoster(poster11);
 
         PostersData[] expected = {poster11, poster10, poster9, poster8, poster7};
-        PostersData [] actual = poster.findLast(5);
+        PostersData [] actual = poster.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+
+    public void shouldFindLast11(){
+        PosterManager poster = new PosterManager(11);
+
+        poster.addPoster(poster1);
+        poster.addPoster(poster2);
+        poster.addPoster(poster3);
+        poster.addPoster(poster4);
+        poster.addPoster(poster5);
+        poster.addPoster(poster6);
+        poster.addPoster(poster7);
+        poster.addPoster(poster8);
+        poster.addPoster(poster9);
+        poster.addPoster(poster10);
+        poster.addPoster(poster11);
+
+        PostersData[] expected = {poster11, poster10, poster9, poster8, poster7, poster6, poster5, poster4, poster3, poster2, poster1};
+        PostersData [] actual = poster.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+
+    public void shouldFindLast12(){
+        PosterManager poster = new PosterManager(12);
+
+        poster.addPoster(poster1);
+        poster.addPoster(poster2);
+        poster.addPoster(poster3);
+        poster.addPoster(poster4);
+        poster.addPoster(poster5);
+        poster.addPoster(poster6);
+        poster.addPoster(poster7);
+        poster.addPoster(poster8);
+        poster.addPoster(poster9);
+        poster.addPoster(poster10);
+        poster.addPoster(poster11);
+
+        PostersData[] expected = {poster11, poster10, poster9, poster8, poster7, poster6, poster5, poster4, poster3, poster2, poster1};
+        PostersData [] actual = poster.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
     }
 }
